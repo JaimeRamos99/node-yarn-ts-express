19286a4508823f7d2b4d7b559c6499f02eade68c
Jaime Ramos <jrossetes@gmail.com>
2021-06-24 22:18:09 -0500

    style: add linter and prettier rules, also add format on save for vscode

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      "plugin:prettier/recommended",
+    ],
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": "error",
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Well done!');
-})
+});
 
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
-})
+app.listen(3000);
