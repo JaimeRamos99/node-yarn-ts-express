166f27ec765520a489d628f4bef20f5875869a24
Jaime Ramos <jrossetes@gmail.com>
2021-06-25 00:14:07 -0500

    chore: remove unnecesary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      "plugin:prettier/recommended",
-    ],
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     ignorePatterns: ['.eslintrc.js'],
     rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      "@typescript-eslint/no-unused-vars": "error",
-      "no-console": "error",
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-console': 'error',
     },
 };
